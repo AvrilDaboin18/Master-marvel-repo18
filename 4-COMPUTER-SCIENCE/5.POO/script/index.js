ab44d80a3824d383208783dd87d50f0a115ce668
Avril18 <maryavrildaboin@gmail.com>
2022-06-20 20:54:09 -0400

    New commit 20/06/22

---
+++ 4-COMPUTER-SCIENCE/5.POO/script/index.js
@@ -0,0 +1,235 @@
+// Sintaxis usando new Object
+// const pokemon = new Object();
+// pokemon.name = 'bulbasaur';
+// pokemon.type = 'grass';
+
+// Sintaxis de iniciador de objeto
+const pokemon = {
+    name: 'bulbasaur',
+    type: 'grass',
+    rankings: { attack: 90, deffense: 80, hp: 100 },
+    color: 'green',
+    height: 1,
+    weight: 5,
+    // No podemos definir metodos
+    // function defiende() {
+    // }
+    attack: function (bonus) { // Funcion que no retorna un valor
+        console.log('latigo cepa!!!', bonus)
+    },
+    run: function() { // Funcion que no retorna un valor
+        console.log('corre lento!!!')
+    },
+    isAlive: function(points) { // Funcion que si retorna un valor
+        if (points > 1) {
+            return true;
+        }
+        return false;
+    }
+};
+
+console.log(pokemon.name);
+pokemon.attack(50);
+console.log(pokemon.isAlive(2));
+
+const sensei = {
+    name: '',
+    teach: function() { // Funcion que no retorna un valor
+        console.log('enseñar Catalogo de APIS y POO');
+    },
+    deliverCode: function() { // Funcion que si retorna un valor
+        const info = { ppt: 'presentacion.pptx', repositorio: 'http:....'};
+        if (info) {
+            return info;
+        }
+        return null;
+    }
+}
+
+sensei.teach();
+const data = sensei.deliverCode();
+data.ppt = 'excel.xls';
+console.log(data);
+
+/**
+ * Los inicialiazadores de objetos no son optimos cuando requerimos
+ * crear muchos objetos porque repetiriamos el codigo y la estrctura 
+ * del objeto.
+ */
+
+// const pokemon2 = {
+//     name: 'ivysaur',
+//     type: 'grass',
+//     rankings: { attack: 95, deffense: 80, hp: 100 },
+//     color: 'green and blue',
+//     height: 1.2,
+//     weight: 55,
+// };
+
+// const pokemon3 = {
+//     name: 'ivysaur',
+//     type: 'grass',
+//     rankings: { attack: 95, deffense: 80, hp: 100 },
+//     color: 'green and blue',
+//     height: 1.2,
+//     weight: 55,
+// };
+
+
+/** Compartir metodos
+ * Se crea el siguiente objeto para compartir metodos para compartir metodos
+*/
+const pokemonMethods = {
+    attack: function (bonus) { 
+        console.log(`${this.power}`, bonus);
+    },
+    isAlive: function(points) {
+        if (points > 1) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**  
+ * Funcion constructora 
+ * Seria una mejora, consiste de una funcion que crea objetos
+ * y los devuelve
+ * */
+function createPokemon(name, type, power) {
+    const pokemon = {
+        name: name,
+        type: type,
+        power: power,
+        attack: pokemonMethods.attack,
+        isAlive: pokemonMethods.isAlive,
+    };
+    // pokemon.attack = function (bonus) { 
+    //     console.log(`${this.power}`, bonus);
+    // };
+    // pokemon.isAlive = function(points) {
+    //     if (points > 1) {
+    //         return true;
+    //     }
+    //     return false;
+    // };
+    return pokemon;
+}
+
+
+const pikachu = createPokemon('pikachu', 'electric', 'thunder');
+console.log('pikachu: ', pikachu);
+pikachu.attack(10);
+
+/** 
+ * Funcion constructora con create object 
+ * Con ayuda de la funcion create de Object podemos crear
+ * objetos con una sintaxis ligeramente mas limpia
+ * */
+function createPokemon2(name, type, power) {
+    const pokemon = Object.create(createPokemon2.prototype);
+    pokemon.name = name;
+    pokemon.type = type;
+    pokemon.power = power;
+    attack = pokemonMethods.attack;
+    isAlive = pokemonMethods.isAlive;
+    return pokemon;
+}
+
+/**
+ * Para agregar metodos compartidos a los objetos nos
+ * valemos de la propiedad prototype que es una propiedad
+ * que todos los objetos que creemos heredad de "arriba"
+ * concretemente desde Object, esto es lo que se conoce como
+ * cadena de prototipos en JS.
+ */
+/**
+ * Prototype y herencia prototipal
+ */
+createPokemon2.prototype.specialAttack =  function () { 
+    console.log('== ataque especial ==');
+};
+
+var pikachu2 = createPokemon2('pikachu', 'electric', 'thunder');
+console.log('pikachu2: ', pikachu2);
+pikachu2.specialAttack();
+
+/**
+ *  Funcion constructora new 
+ * A partir de ES6, se incluyo las keywords this y new.
+ * Una funcion constructora invocado con new, atutomaticamente devuelve un objeto.
+ * This es una palabra reservada que es el objeto que ejecuta la funcion.
+*/
+function Pokemon(name, type, power) {
+    this.name = name;
+    this.type = type;
+    this.power = power;
+    this.attack = pokemonMethods.attack;
+    this.isAlive = pokemonMethods.isAlive;
+};
+
+Pokemon.prototype.specialAttack =  function () { 
+    console.log('== ataque especial ==');
+};
+
+const bulbasaur = new Pokemon('bulbasaur', 'grass', 'razor blades');
+console.log('bulbasaur: ', bulbasaur);
+bulbasaur.attack(20);
+bulbasaur.specialAttack();
+const charmander = new Pokemon('charmander', 'fire', 'flamethower');
+console.log('charmander: ', charmander);
+charmander.attack(30);
+// Objeto sin parametros para el constructor se creará con valores undefined
+// const charizard = new Pokemon();
+// console.log(charizard);
+
+/* Hacer todo eso de arriba class, this, new => sugar syntaxis */
+class Poke {
+    constructor(name, type, power) {
+        this.name = name;
+        this.type = type;
+        this.power = power;
+    }
+    
+    attack(bonus) { 
+        console.log(`${this.power}`, bonus);
+    }
+
+    alive(points) { // Funcion que si retorna un valor
+        if (points > 1) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class FlightPoke extends Poke {
+    constructor(name, type, power, alas) {
+        super(name, type, power);
+        this.alas = alas;
+    }
+    flight() {
+        console.log('yo vuelo');
+    }
+}
+
+class WaterPoke extends Poke {
+    constructor(name, type, power, aletas) {
+        super(name, type, power);
+        this.aletas = aletas;
+    }
+    swim() {
+        console.log('yo nado');
+    }
+}
+
+const squirtle = new WaterPoke('Squirtle', 'Water', 'pulse water', 'tengo 4 aletas');
+const pidgey = new FlightPoke('Pidgey', 'Flight', 'aerial ace', 'tengo 2 alas');
+
+console.log(squirtle)
+squirtle.attack(99);
+squirtle.swim();
+pidgey.attack(10);
+pidgey.flight();
+// squirtle.flight();
+// La linea anterior produce un error porque squirtle no tiene ese metodo
